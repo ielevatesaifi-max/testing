94371b0a8c82850f212b0014614a4331a1829219
ielevate <ielevate.saifi@gmail.com>
2026-05-18 11:12:58 +0530

    new list python

--- web-ap/main.py
+++ web-ap/main.py
@@ -1,1 +1,3 @@
-# insert your code here
+name = 'Version control (or source control) is a system that tracks and manages changes to files over time, recording exactly who made a change, when, and why. It acts as a safety net, allowing developers to revert to previous versions if errors occur and collaborate without overwriting each other’s work'
+l=name.split()
+print(l)
